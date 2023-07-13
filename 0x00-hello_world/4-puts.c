eb78a8a997e710fd4a325439201a9481fa0f0d44
brainiac2025 <afeez.pierscon@gmail.com>
2023-07-13 14:56:24 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	 * Program to print to console with puts function
+	 */
+
+	puts("Programming is like building a multilingual puzzle \n");
+	return (0);
+}
