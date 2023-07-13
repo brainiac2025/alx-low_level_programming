67e795ad65818c0fdf8565e23cfa40d031eb78ce
brainiac2025 <afeez.pierscon@gmail.com>
2023-07-13 15:06:12 -0700

    new change

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
- * program to print hello with 
+ * program to print hello with
  * puts function
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
