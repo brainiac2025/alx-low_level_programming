5cd1fea0a48fb7b5c15cf976d698e03348d11fed
brainiac2025 <afeez.pierscon@gmail.com>
2023-07-18 11:56:12 -0700

    task islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,39 +1,14 @@
-#include <main.h>
-
-int _islower(int c);
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
+#include "main.h"
 
 /**
- * _islower - function to check if an entry is lower case
- *@c: argument to check
- * Return: 1 as lowercase and 0 as not lowercase
+ * _islower - function to check for a small letter
+ * @c: one argument c was pass
+ * Return: 1 for lowercase and 0 for otherwise
  */
 int _islower(int c)
 {
-	int i;
-
-	for (i = 97; i <= 122; i++)
-	{
-		if (c == i)
-			return (1);
-		else
-			return (0);
-	}
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
