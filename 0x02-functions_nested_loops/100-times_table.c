28c9552176fbf55672235d68a9ed7043d7400d45
brainiac2025 <afeez.pierscon@gmail.com>
2023-07-18 19:31:39 -0700

    task 12 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_times_table - function to print times table of n
+ * @n: argument n
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int start;
+	int mult;
+	int inner;
+
+	if (n > 0 && n < 15)
+	{
+		for (start = 0; start <= n; start++)
+                {
+                        for (inner = 0; inner <= n; inner++)
+                        {
+                                mult = start * inner;
+                                if (mult < 10)
+                                {
+                                        _putchar(mult + '0');
+                                }
+				else if (mult > 99)
+				{
+					_putchar((mult / 100) + '0');
+					_putchar(((mult / 10) % 10) + '0');
+					_putchar((mult % 10) + '0');
+				}
+                                else
+                                {
+                                        _putchar((mult / 10) + '0');
+                                        _putchar((mult % 10) + '0');
+                                }
+                                if (start <= n && inner < n )
+                                {
+                                        _putchar(',');
+                                        _putchar(' ');
+                                }
+                        }
+                        _putchar('\n');
+		}
+	}
+}
+
