e789335528c7be4c34749ff91545b16eadf73b66
brainiac2025 <afeez.pierscon@gmail.com>
2023-07-20 15:58:39 -0700

    fizz buzz task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,15 @@ void fizz_buzz(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		else if (num % 5 == 0 && num % 3 != 0)
-			printf("Buzz ");
+			printf(" Buzz");
 		else if (num % 3 == 0 && num % 5 != 0)
-			printf("Fizz ");
+			printf(" Fizz");
+		else if (num == 1)
+			printf("%d", num);
 		else
-			printf("%d ", num);
+			printf(" %d", num);
 	}
 	printf("\n");
 }
