c2388c4196e852237002c74ee6195ca8b25dd8ac
brainiac2025 <afeez.pierscon@gmail.com>
2023-07-24 14:58:15 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,24 +7,21 @@
 void rev_string(char *s)
 {
 	int count, i;
+	char tmp;
 
-	count = 0;
+	tmp = s[0];
 	i = 0;
 
-	/* we need to get the length of the character first*/
-	while (*s != '\0')
+	while (s[count] != '\0')
 	{
-		/*increment the count which is going to represent length*/
 		count++;
-		/* increment address*/
-		s++;
-	}
-	s -=count;
-	/* loop through using the length then print array index of each*/
-
-	while (count > 0)
-	{
-
-		count--;
+		while (i < count)
+		{
+			i++;
+			count--;
+			tmp = s[i];
+			s[i] = s[count];
+			s[count] = tmp;
+		}
 	}
 }
