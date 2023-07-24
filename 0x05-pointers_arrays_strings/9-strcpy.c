ea089cdf29dc31b3bd3efc2203ace239b241065c
brainiac2025 <afeez.pierscon@gmail.com>
2023-07-24 14:08:53 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - function to copy string to destination
+ * @dest: destination argument
+ * @src: source argument
+ * Return: destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+
+	index = 0;
+	while (*src != '\0')
+	{
+		dest[index] = *src;
+		src++;
+		index++;
+	}
+	return (dest);
+}
