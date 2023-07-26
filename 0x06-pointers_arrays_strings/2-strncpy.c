e6468f5740912e87705103b3d96f81188ceb9b13
brainiac2025 <afeez.pierscon@gmail.com>
2023-07-26 16:18:07 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+/**
+ * _strncpy- function to copy string to gether
+ * @dest: destination parameter
+ * @src: source parameter
+ * @n: number of byte parameter
+ * Return: The copy result base on number specify
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	for (j; j < n; j++)
+	{
+		dest[j] = '\0';
+	}
+	return (dest);
+}
