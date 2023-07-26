8c579ae799e48ccf059c0b11ce30433ee3d47790
brainiac2025 <afeez.pierscon@gmail.com>
2023-07-26 15:49:37 -0700

    task 1 correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,16 +7,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	while (*dest)
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
 	{
-		dest++;
+		i++;
 	}
-	while (n)
+	while (j < n && src[j] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
-		n--;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
