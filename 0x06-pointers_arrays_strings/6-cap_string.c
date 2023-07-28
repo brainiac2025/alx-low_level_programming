4e921efdb1fd765e0c443b6ef336f8e1a834593e
brainiac2025 <afeez.pierscon@gmail.com>
2023-07-28 16:52:00 -0700

    task 6 correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,23 +7,34 @@
 char *cap_string(char *str)
 {
 	int index, first;
+	int multipleOperator;
 
 	index = 0;
 	first = 1;
-	while (str[index])
+	multipleOperator = 1;
+	while (str[index] != '\0')
 	{
 		if (first && str[index] >= 'a' && str[index] <= 'z')
 		{
-			str[index] -= 32;
+			if (multipleOperator)
+			{
+				str[index] -= 32;
+				multipleOperator = 0;
+			}
 			first = 0;
 		}
-		else if ((str[index] == ' ' || str[index] == '\t' ||
+		else if (str[index] == ' ' || str[index] == '\t' ||
 str[index] == '\n' || str[index] == ',' || str[index] == ';' ||
 str[index] == '.' || str[index] == '!' || str[index] == '?' ||
 str[index] == '"' || str[index] == '(' || str[index] == ')' ||
-str[index] == '}' || str[index] == '}'))
+str[index] == '}' || str[index] == '}')
 		{
 			first = 1;
+			multipleOperator = 1;
+		}
+		else
+		{
+			multipleOperator = 0;
 		}
 		index++;
 	}
