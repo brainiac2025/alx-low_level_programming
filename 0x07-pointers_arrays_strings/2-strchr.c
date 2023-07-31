3847fceb10c994d4e38612c7fa1e11271bcc908e
brainiac2025 <afeez.pierscon@gmail.com>
2023-07-31 15:25:34 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+/**
+ * _strchr - function that search for character in a string
+ * @s: source string
+ * @c: chracter to search for
+ * Return: the character if found Null otherwise
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	index = 0;
+	while (s[index] != '\0')
+	{
+		if (s[index] == c)
+			return (s + index);
+		index++;
+	}
+	return (s + index);
+}
