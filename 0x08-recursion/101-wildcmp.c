e074b33c8dbbbc6ef54ad5ad434f0a8e899818a6
brainiac2025 <afeez.pierscon@gmail.com>
2023-08-03 11:48:39 -0700

    new correction on task 8 comparing string

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,23 @@
+int wildcmp_h(char *s1, char *s2);
+
 /**
- * wildcmp - function that compares two strings
+ * wildcmp - function that call wildcmp_h
+ * @s1: first parameter
+ * @s2: second parameter
+ * Return: result
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (wildcmp_h(s1, s2));
+}
+
+/**
+ * wildcmp_h - function that compares two strings
  * @s1: first string
  * @s2: second string
  * Return: 1 for identical and 0 otherwise
  */
-int wildcmp(char *s1, char *s2)
+int wildcmp_h(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 	{
@@ -13,28 +26,23 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '*')
 	{
-		while (*(s2 + 1) == '*')
+		if (*(s2 + 1) == '*')
 		{
-			s2++;
+			return (wildcmp_h(s1, s2 + 1));
 		}
 
 		if (*(s2 + 1) == '\0')
 		{
 			return (1);
 		}
-
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-			{
-				return (1);
-			}
-			s1++;
-		}
-		return (0);
+		return(wildcmp_h(s1, s2 + 1) ||
+(*s1 != '\0' && wildcmp_h(s1 + 1, s2)));
 	}
 
 	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
+	{
+		return (wildcmp_h(s1 + 1, s2 + 1));
+	}
+
 	return (0);
 }
