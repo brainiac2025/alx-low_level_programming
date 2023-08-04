bfce09489932bcf3d0f16cbad55da3ab6dd00591
brainiac2025 <afeez.pierscon@gmail.com>
2023-08-04 13:45:09 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function to print number of argument pass
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: Always 0 (success)
+ */
+int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+{
+	int sum, i;
+
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		sum += 1;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
