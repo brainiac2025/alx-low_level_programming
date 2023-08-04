518aca1ff8cf0dda4c3e1160974df3aaf3a1e4ac
brainiac2025 <afeez.pierscon@gmail.com>
2023-08-04 13:24:35 -0700

    task 0 of argc and argv

---
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function to print name of program
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: Always 0 (success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x09-static_libraries/0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+# Dealling with argc and argv
