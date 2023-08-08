29aa305797070eed125ffede37e257c8738bd88f
brainiac2025 <afeez.pierscon@gmail.com>
2023-08-08 15:42:01 -0700

    task 6 first trial

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * len - get the length of the array
+ * @str: the string
+ * Return: the length of the string
+ */
+int len(char *str)
+{
+	int index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		index++;
+	}
+	return (index);
+}
+
+/**
+ * wordl - calculate wordlength
+ * @str: the string
+ * Return: the wordlength
+ */
+int wordl(char *str)
+{
+	int length, wordlen, i;
+
+	wordlen = 0;
+	length = len(str);
+
+	for (i = 0; i < length; i++)
+	{
+		if (str[i] == ' ')
+			wordlen++;
+	}
+	wordlen++;
+	return (wordlen);
+}
+/**
+ * split - split the string
+ * @str: the string
+ * @wordlen: the wordlen
+ * @length: length of the total string
+ * @newStr: pointer to array
+ * Return: void
+ */
+char **split(char *str, int wordlen, int length, char **newStr)
+{
+	int i, j, index, numlen, wordstart;
+
+	index = 0;
+	for (i = 0; i < wordlen; i++)
+	{
+		numlen = 0;
+		while ((index < length) && (str[index] == ' '))
+		{
+			index++;
+		}
+
+		wordstart = index;
+
+		while ((index < length) && (str[index] != ' '))
+		{
+			index++;
+			numlen++;
+		}
+		newStr[i] = malloc(sizeof(char) * (numlen + 1));
+
+		if (newStr[i] == NULL)
+			return (NULL);
+
+		for (j = 0; j < numlen; j++)
+		{
+			newStr[i][j] = str[wordstart + j];
+		}
+		newStr[i][numlen] = '\0';
+	}
+	newStr[i] = NULL;
+	return (newStr);
+}
+
+/**
+ * strtow - function that splits a string into words
+ * @str: the string to split
+ * Return: pointer to the string
+ */
+char **strtow(char *str)
+{
+	int wordlen, length;
+	char **newStr;
+
+	length = len(str);
+	wordlen = wordl(str);
+	newStr = malloc(sizeof(char *) * wordlen);
+	if (newStr == NULL || length == 0)
+	{
+		free(newStr);
+		return (NULL);
+	}
+
+	return (split(str, wordlen, length, newStr));
+}
