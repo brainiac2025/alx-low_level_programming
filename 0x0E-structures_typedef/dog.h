bb75216306b2498a945f4a6dabfe85fed5055e03
brainiac2025 <afeez.pierscon@gmail.com>
2023-08-14 11:06:29 -0700

    task creating struct of dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A struct that define dog properties.
+ *
+ * @name: hold name
+ * @age: hold value of age
+ * @owner: hold the dog owner name
+ *
+ * Description:
+ * The struct represent a dog attributes.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
