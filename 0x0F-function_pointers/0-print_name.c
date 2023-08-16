68cf1575f71a1d4259b77b2717c7a39101137767
brainiac2025 <afeez.pierscon@gmail.com>
2023-08-16 08:01:03 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - function that print name
+ * @name: name to be printed
+ * @f: function that print the name
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(f)(name);
+}
