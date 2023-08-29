3916567324f5b08cb10700b493b4d9ed525fb2ab
brainiac2025 <afeez.pierscon@gmail.com>
2023-08-29 02:02:54 -0700

    new correction on free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,8 +7,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
 	listint_t *temp;
+	listint_t *current = *head;
+
+	if (head == NULL || *head == NULL)
+		exit (1);
 
 	while (current != NULL)
 	{
