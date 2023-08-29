e2390b7fdc230fb1480227ad038fac996901776f
brainiac2025 <afeez.pierscon@gmail.com>
2023-08-29 02:05:33 -0700

    pop node from list code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,9 +10,6 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 	listint_t *current = *head;
 
-	if (head == NULL || *head == NULL)
-		exit (1);
-
 	while (current != NULL)
 	{
 		temp = current;
