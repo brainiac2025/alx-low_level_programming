7b3be2afec5d8b97090a25ae7d0b8e859d6b5f8c
brainiac2025 <afeez.pierscon@gmail.com>
2023-09-04 15:24:46 -0700

    new correction on task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,11 +16,15 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	byte = write(fd, data, strlen(data));
-	if (byte == -1)
+	if (data != NULL)
 	{
-		close(fd);
-		return (-1);
+		byte = write(fd, data, strlen(data));
+
+		if (byte == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
